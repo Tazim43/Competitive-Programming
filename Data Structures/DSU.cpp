84eb1d940c0dfaea09b29039c4412011d500c7a9
Md Tazim Uddin <66694987+TazimAh@users.noreply.github.com>
2022-12-12 02:47:05 +0600

    dsu added

---
+++ Data Structures/DSU.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 2e5+10;
+
+int parent[N];
+int sz[N];
+
+int find(int v){
+    if(find(v)==parent[v])return v;
+    return parent[v] = find(parent[v]); // path compression
+}
+
+void Union(int a, int b){
+    a = find(a);
+    b = find(b);
+    if(a!=b){
+        if(sz[a]<sz[b])swap(a,b); // reduce time 
+        parent[b] = a;
+        sz[a]+=sz[b];
+    }
+    // a==b - cycle found 
+}
+
+int main(){
+    
+    for(int i=0; i<N; i++){
+        parent[i] = i;
+        sz[i] = 1;
+    }
+    
+    
+
+    return 0;
+}
+
